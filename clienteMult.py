0e6cdba5d1ee84a89eae8c5b028bf252728cea12
MarcosAndre5 <marcosandreazevedo@hotmail.com>
2019-05-30 04:07:49 -0300

    fina

--- clienteMult.py
+++ clienteMult.py
@@ -2,36 +2,39 @@ import socket
 import threading
 import sys
 import pickle
+import os
 
 class Cliente():
-    def __init__(self, host="localhost", port=4000):		
-	self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	self.sock.connect((str(host), int(port)))
+	def __init__(self, host="127.0.0.1", port=4000):
+		os.system('clear')
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.sock.connect((str(host), int(port)))
+		msg_recv = threading.Thread(target=self.msg_recv)
+		msg_recv.daemon = True
+		msg_recv.start()
+		print("----- Bem Vindo ao Chat -----")
+		nome = input("Digite seu nome: ")
+		self.send_msg("\n" + nome + " entrou no chat.")
+		while True:
+			msg = input('\nVocê -> ')
+			if msg != 'sair':
+				msg = "\n" + nome + ": " + msg
+				self.send_msg(msg)
+			else:
+				self.send_msg("\n" + nome + " saiu do chat.")
+				self.sock.close()
+				sys.exit()
 
-	msg_recv = threading.Thread(target=self.msg_recv)
+	def msg_recv(self):
+		while True:
+			try:
+				data = self.sock.recv(1024)
+				if data:
+					print(pickle.loads(data))
+			except:
+				pass
 
-	msg_recv.daemon = True
-	msg_recv.start()
-
-	while True:
-            msg = input('->')
-	    if msg != 'sair':
-                self.send_msg(msg)
-            else:
-                self.sock.close()
-		sys.exit()
-
-    def msg_recv(self):
-    	while True:
-            try:
-            	data = self.sock.recv(1024)
-            if data:
-		print(pickle.loads(data))
-            except:
-		pass
-
-    def send_msg(self, msg):
-	self.sock.send(pickle.dumps(msg))
+	def send_msg(self, msg):
+		self.sock.send(pickle.dumps(msg))
 
 c = Cliente()
-		
--- servidorMult.py
+++ servidorMult.py
@@ -2,61 +2,57 @@ import socket
 import threading
 import sys
 import pickle
+import os
 
 class Servidor():
-    def __init__(self, host="localhost", port=4000):
-	self.clientes = []
+	def __init__(self, host="127.0.0.1", port=4000):
+		os.system('clear')
+		self.clientes = [] # Array para clientes
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #
+		self.sock.bind((str(host), int(port)))
+		self.sock.listen(10)
+		self.sock.setblocking(False)
+		aceptar = threading.Thread(target=self.aceptarCon)
+		procesar = threading.Thread(target=self.procesarCon)
+		aceptar.daemon = True
+		aceptar.start()
+		procesar.daemon = True
+		procesar.start()
+		print("----- Servidor da Aplicação -----\n")
+		while True:
+			msg = input('Servidor -> ')
+			if msg == 'sair':
+				self.sock.close()
+				sys.exit()
+			else:
+				pass
 
-	self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	self.sock.bind((str(host), int(port)))
-	self.sock.listen(10)
-	self.sock.setblocking(False)
+	def msg_to_all(self, msg, cliente, id):
+		for c in self.clientes:
+			try:
+				if c != cliente:
+					c.send(msg)
+			except:
+				self.clientes.remove(c)
 
-	aceptar = threading.Thread(target=self.aceptarCon)
-	procesar = threading.Thread(target=self.procesarCon)
-		
-	aceptar.daemon = True
-	aceptar.start()
+	def aceptarCon(self):
+		while True:
+			try:
+				conn, addr = self.sock.accept()
+				conn.setblocking(False)
+				self.clientes.append(conn)
+			except:
+				pass
 
-	procesar.daemon = True
-	procesar.start()
-
-	while True:
-            msg = input('->')
-            if msg == 'sair':
-		self.sock.close()
-		sys.exit()
-            else:
-		pass
-
-    def msg_to_all(self, msg, cliente):
-	for c in self.clientes:
-            try:
-		if c != cliente:
-                    c.send(msg)
-	    except:
-		self.clientes.remove(c)
-
-    def aceptarCon(self):
-	print("aceptarCon iniciado")
-	while True:
-            try:
-		conn, addr = self.sock.accept()
-		conn.setblocking(False)
-		self.clientes.append(conn)
-            except:
-		pass
-
-    def procesarCon(self):
-	print("ProcesarCon iniciado")
-	while True:
-            if len(self.clientes) > 0:
-            for c in self.clientes:
-                try:
-                    data = c.recv(1024)
-                    if data:
-                        self.msg_to_all(data,c)
-		except:
-                    pass
+	def procesarCon(self):
+		while True:
+			if len(self.clientes) > 0:
+				for c in self.clientes:
+					try:
+						data = c.recv(1024)
+						if data:
+							self.msg_to_all(data, c, id)
+					except:
+						pass
 
 s = Servidor()
